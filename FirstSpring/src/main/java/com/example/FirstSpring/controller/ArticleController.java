b537a88def2d4cb2f16f9572451a03fb47d3b98d
chaean <jtkim9965@gmail.com>
2022-07-01 19:07:46 +0900

    first commit

---
+++ FirstSpring/src/main/java/com/example/FirstSpring/controller/ArticleController.java
@@ -0,0 +1,23 @@
+package com.example.FirstSpring.controller;
+
+import com.example.FirstSpring.dto.ArticleForm;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+@Controller
+public class ArticleController {
+
+    @GetMapping("/articles/new")
+    public String newArticleForm() {
+        return "articles/new";
+    }
+
+    @PostMapping("/articles/create")
+    public String createArticle(ArticleForm form) {
+        System.out.println(form.toString());
+        // 1. DTO를 Entity로 변환
+        // 2. 
+        return "";
+    }
+}
